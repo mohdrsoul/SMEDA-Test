0cac3e90add3e2714272bd3216cbda1d828bf60c
MRasoul <MRasoul@DESKTOP-2PQIB62>
2024-02-18 07:42:23 +0400

    Add project files.

---
+++ smdaProject.AppHost/Program.cs
@@ -0,0 +1,7 @@
+using EnvDTE;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.smdaProject>("smdaproject");
+
+builder.Build().Run();
---
+++ smdaProject/Models/SearchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace smdaProject.Models
+{
+
+
+    public class SearchViewModel : PublishedContentWrapped
+    {
+        public SearchViewModel(IPublishedContent content, IPublishedValueFallback publishedValueFallback)
+            : base(content, publishedValueFallback)
+        {
+        }
+
+        public IEnumerable<IPublishedContent> SearchResults { get; set; } = Enumerable.Empty<IPublishedContent>();
+        public bool HasSearched { get; set; }
+    }
+}
